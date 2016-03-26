3b7f8a075e4c2d5d86b9e5f5bbb0f725608ef1c6
Kolyunya <oleynikovny@mail.ru>
2016-03-26 05:34:17 +0300

    Refactored error handler.

--- components/ErrorHandler.php
+++ components/ErrorHandler.php
@@ -3,6 +3,7 @@
 namespace app\components;
 
 use Yii;
+use Exception;
 use app\responses\error\CommonErrorResponse;
 
 /**
@@ -16,9 +17,24 @@ class ErrorHandler extends \yii\web\ErrorHandler
      */
     protected function renderException($exception)
     {
-        $message = YII_DEBUG ? $exception->getMessage() : null;
+        $message =
+            YII_DEBUG === true ?
+            $this->constructMessage($exception) :
+            null;
         $response = new CommonErrorResponse($message);
         Yii::$app->response->content = $response;
         Yii::$app->response->send();
+    }
+
+    /**
+     * Constructs an error message.
+     * @return string
+     */
+    private function constructMessage(Exception $exception)
+    {
+        $message = $exception->getMessage();
+        $message .= ' in file ' . $exception->getFile();
+        $message .= ' at line ' . $exception->getLine();
+        return $message;
     }
 }
